80dab55c0c9568ff2781f274acb427096dc407d5
armanbaghajyan <armanbaxajyan@gmail.com>
2017-05-05 11:00:44 +0000

    added enfirement versioning and roles

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,11 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello Chef Config !!'
+	content 'Hello Chef Config !! v2.0.0'
+end
+
+
+webnodes = search('node', 'role:web')
+
+webnodes.each do |node|
+	puts node
 end
